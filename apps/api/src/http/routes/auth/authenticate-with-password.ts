9182aa07a8413799e83444307dc31415f9f94198
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-03 11:18:58 -0300

    auth with password + jwt

---
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -0,0 +1,52 @@
+import { compare } from 'bcryptjs'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function authenticateWithPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/sessions/password',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Authenticate with email and password',
+        body: z.object({
+          email: z.email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { email, password } = request.body
+
+      const user = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (!user) {
+        return reply.status(400).send({ message: 'Invalid email or password' })
+      }
+
+      if (user.passwordHash === null) {
+        return reply
+          .status(400)
+          .send({ message: 'User has no password set. Use social login' })
+      }
+
+      const isPasswordValid = await compare(password, user.passwordHash)
+
+      if (!isPasswordValid) {
+        return reply.status(400).send({ message: 'Invalid email or password' })
+      }
+
+      const token = await reply.jwtSign(
+        { sub: user.id },
+        { sign: { expiresIn: '7d' } },
+      )
+
+      return reply.status(201).send({ token })
+    },
+  )
+}
