619c2e1081427f3e2b6a44979475fbc4f1f152b7
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-03 20:24:21 -0300

    api: routes get org and org

---
+++ apps/api/src/http/routes/orgs/get-organization.ts
@@ -0,0 +1,43 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+
+export async function getOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get details of an organization where user is member',
+          security: [{ bearerAuth: [] }],
+          params: z.object({ slug: z.string() }),
+          response: {
+            200: z.object({
+              organization: z.object({
+                id: z.string(),
+                name: z.string(),
+                slug: z.string(),
+                domain: z.string().nullish(),
+                attachUsersByDomain: z.boolean(),
+                avatarUrl: z.string().nullish(),
+                createdAt: z.date(),
+                updatedAt: z.date(),
+                ownerId: z.uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const { organization } = await request.getUserMembership(slug)
+
+        return reply.send({ organization })
+      },
+    )
+}
---
+++ apps/api/src/http/routes/orgs/get-organizations.ts
@@ -0,0 +1,60 @@
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+export async function getOrganizations(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get organizations where user is member',
+          security: [{ bearerAuth: [] }],
+          response: {
+            200: z.object({
+              organizations: z.array(
+                z.object({
+                  name: z.string(),
+                  id: z.string(),
+                  slug: z.string(),
+                  avatarUrl: z.string().nullable(),
+                  role: roleSchema,
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const organizations = await prisma.organization.findMany({
+          select: {
+            id: true,
+            name: true,
+            slug: true,
+            avatarUrl: true,
+            members: {
+              select: { role: true },
+              where: { userId },
+            },
+          },
+          where: { members: { some: { userId } } },
+        })
+
+        const orgsWithRoles = organizations.map(({ members, ...org }) => ({
+          ...org,
+          role: members[0]?.role,
+          members: undefined,
+        }))
+
+        return reply.status(200).send({ organizations: orgsWithRoles })
+      },
+    )
+}
