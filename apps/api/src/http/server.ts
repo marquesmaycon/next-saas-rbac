aadcee6489192c6fc5169db3c5696cdd7718bcfb
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-04 11:20:45 -0300

    api: routes create and delete project

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -25,6 +25,7 @@ import { getOrganizations } from './routes/orgs/get-organizations'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
+import { createProject } from './routes/projects/create-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -74,6 +75,8 @@ app.register(getMembership)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
 app.register(transferOrganization)
+
+app.register(createProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`HTTP server running on http://localhost:${env.SERVER_PORT}`)
